81f57c46227b6751128eb86ca36b49cc20d1be5d
AnnaAntr <AnnaAntr@users.noreply.github.com>
2026-02-26 21:28:51 +0300

    filling table with data added

---
+++ src/FileManager.java
@@ -0,0 +1,82 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class FileManager {
+
+    private RandomAccessFile file;
+
+    // "C:\\Users\\aantrushina\\Documents\\test.txt"
+
+    FileManager(String path) {
+        try {
+            file = new RandomAccessFile(path, "rw");
+        } catch (FileNotFoundException e) {
+            System.out.println("error in constructor");
+        }
+    }
+
+
+
+    public List<String> readByteArray(/*int numberOfBytes, int start*/) {
+        List<String> bytes = new ArrayList<>();
+
+
+        try {
+            int b;
+            while ((b = file.read()) != -1) {
+
+                bytes.add(String.format("%02X", b));
+
+                //System.out.println(b + " | hex: " + String.format("%02X", b));
+            }
+
+        } catch (IOException e) {
+            System.out.println("error during reading array");
+        }
+
+
+        closeFile();
+
+        return bytes;
+    }
+
+
+    public String readOneByte(int position) {
+        int b = -1;
+
+        try {
+            file.seek(position);
+            b = file.read();
+            //System.out.println(b);
+        } catch (IOException e) {
+            System.out.println("error during reading byte");
+        }
+
+        return String.format("%02X", b);
+    }
+
+
+    public long getSize() {
+        try {
+            return file.length();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+
+
+    public void closeFile() {
+        try {
+            file.close();
+        } catch (IOException e) {
+            //throw new RuntimeException(e);
+            System.out.println("error during closing file");
+        }
+    }
+}
--- src/UserInterface.java
+++ src/UserInterface.java
@@ -1,10 +1,18 @@
 import javax.swing.*;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 import javax.swing.table.AbstractTableModel;
 import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.TableCellRenderer;
 import javax.swing.table.TableColumn;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
+import java.io.File;
 
 public class UserInterface {
 
@@ -21,13 +29,14 @@ public class UserInterface {
 
     public static void createGUI() {
         JFrame frame = new JFrame("Frame");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         Toolkit toolkit = Toolkit.getDefaultToolkit();
         Dimension screenSize = toolkit.getScreenSize();
+        int frameWidth = (int) (screenSize.width * 0.6);
+        int frameHeight = (int) (screenSize.height * 0.6);
 
-
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(screenSize.width - 800, screenSize.height - 500);
+        frame.setSize(frameWidth, frameHeight);
         frame.setVisible(true);
 
 
@@ -35,6 +44,7 @@ public class UserInterface {
 
 
         JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, createLeftSide(), createRightSide());
+        splitPane.setDividerLocation((int) (frameWidth * 0.7));
         frame.add(splitPane);
 
 
@@ -44,19 +54,31 @@ public class UserInterface {
     public static JMenuBar createMenuBar() {
         JMenuBar menuBar = new JMenuBar();
 
+        JFileChooser fileChooser = new JFileChooser();
+
+
 
         JMenu file = new JMenu("Файл");
         JMenuItem open = new JMenuItem("Открыть");
         file.add(open);
 
-//        open.addActionListener(new ActionListener() {
-//            @Override
-//            public void actionPerformed(ActionEvent e) {
-//
-//            }
-//        });
+        open.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int userChoice = fileChooser.showDialog(null, "Открыть");
 
-        // adding
+                if (userChoice == JFileChooser.APPROVE_OPTION) {
+                    String filePath = fileChooser.getSelectedFile().getAbsolutePath().replaceAll("\\\\", "\\\\\\\\");
+
+                    MainTableModel tableModel = (MainTableModel) table.getModel();
+                    tableModel.setFileManager(filePath);
+                }
+            }
+        });
+
+
+
+
         menuBar.add(file);
 
         return menuBar;
@@ -69,9 +91,9 @@ public class UserInterface {
 
         TableColumn col0 = table.getColumnModel().getColumn(0);
         //col0.setHeaderValue("");
-        col0.setMaxWidth(50);
+        col0.setMaxWidth(30);
         col0.setMinWidth(20);
-        col0.setPreferredWidth(40);
+        col0.setPreferredWidth(30);
 
         DefaultTableCellRenderer centerRenderer = new DefaultTableCellRenderer();
         centerRenderer.setHorizontalAlignment(SwingConstants.CENTER);
@@ -81,7 +103,32 @@ public class UserInterface {
         table.setRowSelectionAllowed(true);
         table.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
 
-        //table.setColumnSelectionInterval(1, table.getColumnCount() - 1);
+        ListSelectionModel selectionModel = table.getSelectionModel();
+        selectionModel.addListSelectionListener(new ListSelectionListener() {
+            @Override
+            public void valueChanged(ListSelectionEvent e) {
+                int[] selectedRows = table.getSelectedRows();
+                // TODO
+
+            }
+        });
+
+
+//        table.addMouseMotionListener(new MouseMotionAdapter() {
+//            @Override
+//            public void mouseMoved(MouseEvent e) {
+//                int row = table.rowAtPoint(e.getPoint());
+//                int column = table.columnAtPoint(e.getPoint());
+//                Object cellValue = table.getValueAt(row, column);
+//                //System.out.println(cellValue);
+//
+//                JToolTip cellTip = new JToolTip();
+//
+//            }
+//        });
+
+        table.getColumnModel().getColumn(0).setCellRenderer(new MainCellRenderer());
+
 
 
         return new JScrollPane(table);
@@ -91,11 +138,25 @@ public class UserInterface {
         JPanel rightPanel = new JPanel();
         //rightPanel.setLayout(new BorderLayout(BorderLayout.CENTER));
 
-        SpinnerModel spinnerModel = new SpinnerNumberModel(10, 2, 30, 1);
+//        SpinnerModel spinnerModel = new SpinnerNumberModel(10, 2, 30, 1);
+//        JSpinner spinnerColumns = new JSpinner(spinnerModel);
+//
+//        rightPanel.add(new JLabel("Количество столбцов:"));
+//        rightPanel.add(spinnerColumns);
+
+        rightPanel.setLayout(new BoxLayout(rightPanel, BoxLayout.Y_AXIS));
+        rightPanel.add(Box.createHorizontalGlue());
+
+        SpinnerModel spinnerModel = new SpinnerNumberModel(table.getColumnCount() - 1, 2, 30, 1);
         JSpinner spinnerColumns = new JSpinner(spinnerModel);
 
-        rightPanel.add(new JLabel("Количество столбцов:"));
-        rightPanel.add(spinnerColumns);
+        JPanel columnsPanel = new JPanel();
+        columnsPanel.add(new JLabel("Количество столбцов"));
+        columnsPanel.add(spinnerColumns);
+        //columnsPanel.setBounds(10, 50, 50, 50);
+        //columnsPanel.setBounds(BorderFactory.createEmptyBorder(50, 10, 10, 10));
+
+        rightPanel.add(columnsPanel);
 
         spinnerColumns.addChangeListener(new ChangeListener() {
             @Override
@@ -104,24 +165,58 @@ public class UserInterface {
 
                 MainTableModel tableModel = (MainTableModel) table.getModel();
                 tableModel.setColumnCount(newValue);
-
-
-                // TODO перерисовать таблицу (данные) ???
             }
         });
 
 
         // TODO здесь будет просмотр значения блоков данных
 
-        JTextField usIntField = new JTextField(15);
-        JTextField sIntField = new JTextField(15);
+        JLabel usIntLabel = new JLabel("Unsigned Int");
+        //usIntLabel.setHorizontalAlignment(JLabel.LEFT);
+        JLabel sIntLabel = new JLabel("Signed Int");
+        JLabel floatLabel = new JLabel("Float");
+        JLabel doubleLabel = new JLabel("Double");
+
+        JTextField usIntField = new JTextField(Integer.toString(table.getColumnCount()), 15);
+        JTextField sIntField = new JTextField("text", 15);
         JTextField floatField = new JTextField(15);
         JTextField doubleField = new JTextField(15);
 
+
+
+        rightPanel.add(usIntLabel);
         rightPanel.add(usIntField);
+        usIntField.setMaximumSize(new Dimension(200, 35));
+        //usIntField.setAlignmentX(Component.CENTER_ALIGNMENT);
+        usIntLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+
+        rightPanel.add(sIntLabel);
         rightPanel.add(sIntField);
+        sIntField.setMaximumSize(new Dimension(200, 35));
+        //sIntField.setAlignmentX(Component.CENTER_ALIGNMENT);
+        sIntLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+
+        rightPanel.add(floatLabel);
         rightPanel.add(floatField);
+        floatField.setMaximumSize(new Dimension(200, 35));
+        //floatField.setAlignmentX(Component.CENTER_ALIGNMENT);
+        floatLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        rightPanel.add(doubleLabel);
         rightPanel.add(doubleField);
+        doubleField.setMaximumSize(new Dimension(200, 35));
+        //doubleField.setAlignmentX(Component.CENTER_ALIGNMENT);
+        doubleLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+
+
+        Dimension minSize = new Dimension(5, 200);
+        Dimension prefSize = new Dimension(5, 300);
+        Dimension maxSize = new Dimension(Short.MAX_VALUE, 100);
+
+        rightPanel.add(new Box.Filler(minSize, prefSize, maxSize));
 
 
 
@@ -136,10 +231,17 @@ public class UserInterface {
 
 class MainTableModel extends AbstractTableModel {
     private int rowCount = 40;
-    private int columnCount = 10;
+    private int columnCount = 11;
+
+    private FileManager fileManager = null;
 
     public void setColumnCount(int newColumnCount) {
-        this.columnCount = newColumnCount;
+        this.columnCount = newColumnCount + 1;
+        fireTableStructureChanged();
+    }
+
+    public void setFileManager(String path) {
+        this.fileManager = new FileManager(path);
         fireTableStructureChanged();
     }
 
@@ -156,7 +258,18 @@ class MainTableModel extends AbstractTableModel {
     @Override
     public Object getValueAt(int rowIndex, int columnIndex) {
         if (columnIndex == 0)
-            return rowIndex;
+            return rowIndex * (getColumnCount() - 1);
+
+        if (fileManager != null) {
+            int position = rowIndex * (getColumnCount() - 1) + columnIndex - 1;
+
+            if (position < fileManager.getSize()) {
+                return fileManager.readOneByte(position);
+            }
+        }
+
+        // TODO добавлять строки, если файл большой
+
         return null;
     }
 
@@ -178,20 +291,15 @@ class MainTableModel extends AbstractTableModel {
 //    }
 }
 
-//class FirstColumnModel extends AbstractTableModel {
-//
-//    @Override
-//    public int getRowCount() {
-//        return 40;
-//    }
-//
-//    @Override
-//    public int getColumnCount() {
-//        return 1;
-//    }
-//
-//    @Override
-//    public Object getValueAt(int rowIndex, int columnIndex) {
-//        return rowIndex;
-//    }
-//}
+class MainCellRenderer extends DefaultTableCellRenderer {
+    @Override
+    public Component getTableCellRendererComponent(JTable table, Object value,
+                                                   boolean isSelected, boolean hasFocus, int row, int column) {
+        super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+
+        if (value != null)
+            setToolTipText(String.valueOf(Integer.parseInt(value.toString(), 16)));
+
+        return this;
+    }
+}
